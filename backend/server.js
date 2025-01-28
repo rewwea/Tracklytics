9bcf625a12b4f3d9d914ae72d44883eedab44a7d
rewwea <avandronov10@gmail.com>
2025-01-28 21:52:29 +0500

    update server port to 5005 and add package.json for project dependencies

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
     res.send("Tracklytics API is running...");
 });
 
-const PORT = 5000;
+const PORT = 5005;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
